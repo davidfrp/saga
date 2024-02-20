44fc3d5818ae0dbcece0c880035c714d132d571a
David Petersen <davidfrp@outlook.dk>
2024-02-20 20:37:37 +0100

    refactor: improved fuzzy search and list of issues

--- src/prompts/askIssue.ts
+++ src/prompts/askIssue.ts
@@ -2,31 +2,48 @@ import inquirer from "inquirer"
 import inquirerPrompt from "inquirer-autocomplete-prompt"
 import chalk from "chalk"
 import { Issue } from "../@types/atlassian.js"
-import { getSourceFn } from "./utils.js"
+import createSourceFn from "./generator/sourceFn.js"
 
 export default async function (issues: Issue[]): Promise<Issue> {
   inquirer.registerPrompt("autocomplete", inquirerPrompt)
+
   const { issue } = await inquirer.prompt([
     {
       type: "autocomplete",
       name: "issue",
       message: "Select an issue",
-      source: getSourceFn(issues, {
-        columns: [
-          "key",
-          (issue) => {
-            if (!issue.fields.issuetype.color)
-              return issue.fields.issuetype.name
+      source: createSourceFn(issues, {
+        meta: (issue) => {
+          const meta = [
+            issue.fields.issuetype.name,
+            issue.fields.parent?.fields.summary,
+            issue.fields.parent?.key,
+          ]
 
-            return chalk.hex(issue.fields.issuetype.color)(
-              issue.fields.issuetype.name,
-            )
+          return meta.filter(Boolean).join(" ")
+        },
+        columns: [
+          {
+            value: (issue) => {
+              return chalk.hex(issue.fields.issuetype.color ?? "")(issue.key)
+            },
           },
-          (issue) => issue.fields.status.name,
-          (issue) => issue.fields.assignee?.emailAddress.split("@")[0] ?? "",
-          (issue) => issue.fields.summary,
+          {
+            value: (issue) => issue.fields.priority.name,
+          },
+          {
+            value: (issue) => issue.fields.status.name,
+          },
+          {
+            value: (issue) =>
+              issue.fields.assignee?.emailAddress.split("@")[0] ??
+              chalk.dim("None"),
+          },
+          {
+            value: (issue) => issue.fields.summary,
+            maxWidth: 50,
+          },
         ],
-        columnSpacing: 2,
       }),
     },
   ])
